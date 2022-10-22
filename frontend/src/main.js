39e7307e4f537906b93c1d9a13f1f0ab6e90a80a
RomanShirov <RomanShirov.dev@yandex.ru>
2022-10-22 11:31:39 +0300

    Add Initial View

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import vuetify from './plugins/vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
---
+++ frontend/src/views/NoteEditor.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "NotesEditor"
+}
+</script>
+
+<style scoped>
+
+</style>
