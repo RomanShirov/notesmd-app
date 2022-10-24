88d97d74b498b42010c522922823ba7853199af4
RomanShirov <RomanShirov.dev@yandex.ru>
2022-10-24 19:05:04 +0300

    Add Markdown Editor

--- frontend/src/components/NoteEditor/Editor.vue
+++ frontend/src/components/NoteEditor/Editor.vue
@@ -1,19 +1,38 @@
 <template>
-  <div class="editor">
-    <h1 class="title">{{note.title}}</h1>
-    <p class="title" v-html="note.content"/>
-    <p class="title">{{note}}</p>
+  <div class="editor-container">
+    <md-editor
+        class="editor"
+        v-model="note"
+        theme="dark"
+        language="en-US"
+        toolbarsExclude="[github]"
+        codeTheme="atom"
+    />
+<!--    <h1 class="title">{{note.title}}</h1>-->
+<!--    <p class="title" v-html="note.content"/>-->
+<!--    <p class="title">{{note}}</p>-->
   </div>
 </template>
 
 <script>
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
 export default {
   name: "Editor",
+  components: {
+    MdEditor
+  },
   data() {
     return {
-      note: this.$store.state.currentNote
+      note: "123"
     }
   },
+  computed: {
+    getNoteFromState: function (){
+      this.note = this.$store.state.currentNote.content
+    }
+  }
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 import vuetify from './plugins/vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import { loadFonts } from './plugins/webfontloader'
+import {loadFonts} from './plugins/webfontloader'
 
 loadFonts()
 
 createApp(App)
-  .use(router)
-  .use(store)
-  .use(vuetify)
-  .mount('#app')
+    .use(router)
+    .use(store)
+    .use(vuetify)
+    .mount('#app')
