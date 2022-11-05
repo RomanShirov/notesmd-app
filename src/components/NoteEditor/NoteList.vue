c5d43b82c82b8fb8fa19fa112b5191cfbb012706
RomanShirov <RomanShirov.dev@yandex.ru>
2022-11-05 19:44:07 +0300

    Fix folder selecting

--- src/components/NoteEditor/NoteList.vue
+++ src/components/NoteEditor/NoteList.vue
@@ -1,17 +1,18 @@
 <template>
   <v-list class="card" lines="two">
     <div class="note-list-header">
-      <v-tabs class="tab-list" v-model="folders" background-color="red-lighten-2" height="50" hide-slider>
-        <v-tab class="tab rounded-lg" v-for="(folder, idx) in folders" :key="folder" :value="folder" @click="updateSelectedFolder(folder)">
+      <v-tabs class="tab-list" v-model="notes.folders" background-color="red-lighten-2" height="50" hide-slider>
+        <v-tab class="tab rounded-lg" v-for="folder in notes.folders" :key="folder" :value="folder"
+               @click="updateSelectedFolder(folder)">
           {{ folder }}
         </v-tab>
       </v-tabs>
     </div>
-    <p class="note-list-title">{{selectedFolder}}</p>
+    <p class="note-list-title">{{ notes.selectedFolder }}</p>
     <v-list-item
         @click="updateDisplayInfo(item)"
         class="element"
-        v-for="item in notes"
+        v-for="item in notes.notes"
         :key="item.id"
         :title="item.title"
         :subtitle="item.data"
@@ -25,10 +26,7 @@ export default {
   name: "NoteList",
   data() {
     return {
-      selectedFolder: '',
       selectedNoteId: '',
-      notes: {},
-      folders: [],
     }
   },
   beforeMount() {
@@ -36,6 +34,7 @@ export default {
     this.folders = this.$store.state.selectedObjectState.folders
     this.selectedFolder = this.$store.state.selectedObjectState.selectedFolder
   },
+
   methods: {
     updateDisplayInfo: function (item) {
       this.selectedNoteId = item.id
@@ -43,21 +42,21 @@ export default {
     },
 
     updateSelectedFolder: function (item) {
-      this.selectedFolder = item
       this.$store.commit('selectFolder', item);
       this.$store.commit('setNotesByFolder', item);
-      this.notes = this.$store.state.receivedFolderData
     },
   },
   computed: {
-    // lastSyncTime: function () {
-    //   return this.$store.getters.getLastSyncTime.toLocaleString()
-    // },
-    computed: {
-      getFolders: function () {
-        return this.folders
-      },
-    }
+    notes() {
+      return {
+        notes: this.$store.state.receivedFolderData,
+        folders: this.$store.state.selectedObjectState.folders,
+        selectedFolder: this.$store.state.selectedObjectState.selectedFolder
+      }
+    },
+    getFolders: function () {
+      return this.folders
+    },
   }
 }
 </script>
