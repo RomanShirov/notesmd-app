e52aeb1be929ba8d36152749a31f618e10fab999
RomanShirov <RomanShirov.dev@yandex.ru>
2022-11-16 05:00:53 +0300

    Add folder select when creating

--- frontend/src/components/NoteEditor/CreateNoteModal.vue
+++ frontend/src/components/NoteEditor/CreateNoteModal.vue
@@ -5,6 +5,17 @@
         <h1 class="cn-title">Create note</h1>
         <input type="text" class="input" v-model="createRequest.title" placeholder="Title"/>
         <input type="text" class="input" @keyup.enter="uploadNote" v-model="createRequest.folder" placeholder="Folder"/>
+        <v-card class="select">
+          <v-select
+              v-model="createRequest.folder"
+              label="Or select existing folder"
+              :items="folders"
+              variant="solo"
+              theme="dark"
+              bg-color="#323234"
+              hide-details
+          ></v-select>
+        </v-card>
         <div class="cn-btn-group">
           <input type="button" class="cn-btn" value="Create" @click="uploadNote"/>
           <input type="button" class="cn-btn" value="Close" @click="$store.commit('setModalVisibility');"/>
