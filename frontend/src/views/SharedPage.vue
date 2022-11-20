8c2073ecd9ff1ce30d3bdb22a5742d345e018460
RomanShirov <RomanShirov.dev@yandex.ru>
2022-11-21 01:53:54 +0300

    Edit shared template

--- frontend/src/views/SharedPage.vue
+++ frontend/src/views/SharedPage.vue
@@ -1,20 +1,45 @@
 <template>
-  <h1>Test</h1>
-  <h3>{{ user }} {{ noteSharedId }}</h3>
+  <div class="app-container">
+    <md-editor
+        class="editor readonly"
+        ref="editor"
+        v-model="data"
+        theme="dark"
+        language="en-US"
+        codeTheme="atom"
+        previewOnly="true"
+    ></md-editor>
+  </div>
 </template>
 
 <script>
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
 export default {
   name: 'SharedPage',
+  components: {
+    MdEditor,
+  },
   data(){
     return{
       user: this.$route.params.user,
-      noteSharedId: this.$route.params.noteSharedId
+      noteSharedId: this.$route.params.noteSharedId,
+      title: '',
+      data: '# Title\n' + `#### Published by ${this.$route.params.user} \n`
     }
   },
 };
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+@import '../styles/NoteEditor/Editor'
+.app-container
+  flex-direction: column
+  background: #1b1d1f
 
+.editor
+  padding: 0 0 0 50px
+  border-radius: 15px
+  width: 50%
 </style>
